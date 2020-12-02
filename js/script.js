6432ead94579ad6f2edea4e19d4a3cdb8d7664df
Damian Fusiek <damas13251@gmail.com>
2020-12-02 20:53:50 +0100

    fix errors

--- js/script.js
+++ js/script.js
@@ -1,9 +1,6 @@
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
-
 console.log('Gracz wpisał: ' + playerInput);
-
 let playerMove = 'nieznany ruch';
-
 if (playerInput == '1') {
     playerMove = 'kamień';
 } else if (playerInput == '2') {
@@ -12,5 +9,30 @@ if (playerInput == '1') {
     playerMove = 'nożyce';
 }
 
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+let computerMove = 'nieznany ruch';
+if (randomNumber == 1) {
+    computerMove = 'papier';
+} else if (randomNumber == 2) {
+    computerMove = 'kamień';
+} else {
+    computerMove = 'nożyce'
+}
+printMessage('Ty dałeś: ' + playerMove + '<br> Komputer dał: ' + computerMove)
 
 printMessage('Twój ruch to: ' + playerMove);
+if (computerMove == 'kamień' && playerMove == 'papier') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'nożyce') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'nożyce' && playerMove == 'kamień') {
+    printMessage('Ty wygrywasz!');
+} else if (computerMove == 'papier' && playerMove == 'kamień') {
+    printMessage('Wygrywa komputer!');
+} else if (computerMove == 'nożyce' && playerMove == 'papier') {
+    printMessage('Wygrywa komputer!');
+} else if (computerMove == 'kamień' && playerMove == 'nożyce') {
+    printMessage('Wygrywa komputer!');
+} else {
+    printMessage('REMIS!');
+}
